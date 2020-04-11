cc315719510d340e01fe9f4afbf871e85bb2013e
Jaewon Noh <opqw987@gmail.com>
2020-04-11 21:42:16 +0900

    Alamofire 유틸리티 추가

---
+++ GithubSearch/Utilities/AlamofireUtil.swift
@@ -0,0 +1,40 @@
+//
+//  AlamofireUtil.swift
+//  GithubSearch
+//
+//  Created by jaewon on 2020/04/11.
+//  Copyright © 2020 jaewon. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+import ObjectMapper
+import AlamofireObjectMapper
+
+class AlamofireUtil {
+    static let shared: AlamofireUtil = AlamofireUtil()
+    
+    func getDataRequest(_ api: API,
+                        headers: HTTPHeaders? = nil) -> DataRequest {
+        return Alamofire.request(api.url,
+                                 method: api.method,
+                                 parameters: api.parameters,
+                                 encoding: URLEncoding.default,
+                                 headers: headers)
+    }
+    
+    func requestObject<T: Mappable>(_ api: API,
+                                    headers: HTTPHeaders? = nil,
+                                    onSuccess: @escaping (_ response: T) -> (),
+                                    onError: @escaping (_ error: Error) -> ()) {
+        self.getDataRequest(api, headers: headers)
+            .responseObject { (dataResponse: DataResponse<T>) in
+                switch dataResponse.result {
+                case .success(let data):
+                    onSuccess(data)
+                case .failure(let error):
+                    onError(error)
+                }
+        }
+    }
+}
