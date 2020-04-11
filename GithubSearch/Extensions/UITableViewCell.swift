b68d4591f89a09be6949a2d9567ee20de22167c3
Jaewon Noh <opqw987@gmail.com>
2020-04-11 19:36:30 +0900

    UITableViewCell extension 추가

---
+++ GithubSearch/Extensions/UITableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  UITableViewCell.swift
+//  GithubSearch
+//
+//  Created by jaewon on 2020/04/11.
+//  Copyright © 2020 jaewon. All rights reserved.
+//
+
+import UIKit
+
+extension UITableViewCell {
+    static var identifier: String {
+        return String(describing: self)
+    }
+}
+
