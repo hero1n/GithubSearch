ffe780a45f35e65cec3bcd686231ca8df6fbc9d5
Jaewon Noh <opqw987@gmail.com>
2020-04-11 18:03:31 +0900

    API 서비스 추가

---
+++ GithubSearch/Constants/URLConstants.swift
@@ -0,0 +1,13 @@
+//
+//  URLConstants.swift
+//  GithubSearch
+//
+//  Created by jaewon on 2020/04/11.
+//  Copyright © 2020 jaewon. All rights reserved.
+//
+
+import Foundation
+
+class URLConstants {
+    public static var BASE_URL: String = "https://api.github.com"
+}
---
+++ GithubSearch/Services/API.swift
@@ -0,0 +1,37 @@
+//
+//  API.swift
+//  GithubSearch
+//
+//  Created by jaewon on 2020/04/11.
+//  Copyright © 2020 jaewon. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+enum API {
+    case getUsers(query: String)
+    case getSingleUser(userName: String)
+    
+    var method: HTTPMethod {
+        switch self {
+        case .getUsers, .getSingleUser: return .get
+        }
+    }
+    
+    var url: URLConvertible {
+        switch self {
+        case .getUsers: return "\(URLConstants.BASE_URL)/search/users"
+        case .getSingleUser(let userName): return "\(URLConstants.BASE_URL)/users/\(userName)"
+        }
+    }
+    
+    var parameters: Parameters {
+        switch self {
+        case .getUsers(let query):
+            return ["q": query]
+        default:
+            return [:]
+        }
+    }
+}
