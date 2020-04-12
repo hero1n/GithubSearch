27d560b4d5e1a7915f784085714dcccc12390898
Jaewon Noh <opqw987@gmail.com>
2020-04-12 16:38:20 +0900

    UIImage, UISearchBar Extension 추가

---
+++ GithubSearch/Extensions/UIImage.swift
@@ -0,0 +1,25 @@
+//
+//  UIImage.swift
+//  GithubSearch
+//
+//  Created by jaewon on 2020/04/12.
+//  Copyright © 2020 jaewon. All rights reserved.
+//
+
+import UIKit
+
+extension UIImage {
+    func imageWithPixelSize(size: CGSize, filledWithColor color: UIColor = UIColor.clear, opaque: Bool = false) -> UIImage? {
+        return imageWithSize(size: size, filledWithColor: color, scale: 1.0, opaque: opaque)
+    }
+
+    func imageWithSize(size: CGSize, filledWithColor color: UIColor = UIColor.clear, scale: CGFloat = 0.0, opaque: Bool = false) -> UIImage? {
+        let rect = CGRect.init(x: 0, y: 0, width: size.width, height: size.height)
+        UIGraphicsBeginImageContextWithOptions(size, opaque, scale)
+        color.set()
+        UIRectFill(rect)
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return image
+    }
+}
---
+++ GithubSearch/Extensions/UISearchBar.swift
@@ -0,0 +1,96 @@
+//
+//  UISearchBar.swift
+//  GithubSearch
+//
+//  Created by jaewon on 2020/04/12.
+//  Copyright © 2020 jaewon. All rights reserved.
+//
+
+import UIKit
+
+/// Add UISearchBar Activity Indicator  https://maysamsh.me/2018/07/30/ios-show-activity-indicator-in-uisearchbar/
+extension UISearchBar {
+    private var textField: UITextField? {
+        let subViews = self.subviews.flatMap { $0.subviews }
+        if #available(iOS 13, *) {
+            if let _subViews = subViews.last?.subviews {
+                return (_subViews.filter { $0 is UITextField }).first as? UITextField
+            } else {
+                return nil
+            }
+            
+        } else {
+            return (subViews.filter { $0 is UITextField }).first as? UITextField
+        }
+        
+    }
+    
+    private var searchIcon: UIImage? {
+        let subViews = subviews.flatMap { $0.subviews }
+        return ((subViews.filter { $0 is UIImageView }).first as? UIImageView)?.image
+    }
+    
+    private func getViewElement<T>(type: T.Type) -> T? {
+        let svs = subviews.flatMap { $0.subviews }
+        guard let element = (svs.filter { $0 is T }).first as? T else { return nil }
+        return element
+    }
+    
+    func getSearchBarTextField() -> UITextField? {
+        return getViewElement(type: UITextField.self)
+    }
+    
+    func setTextColor(color: UIColor) {
+        if let textField = getSearchBarTextField() {
+            textField.textColor = color
+        }
+    }
+    
+    func setTextFieldColor(color: UIColor) {
+        if let textField = getViewElement(type: UITextField.self) {
+            switch searchBarStyle {
+            case .minimal:
+                textField.layer.backgroundColor = color.cgColor
+                textField.layer.cornerRadius = 6
+            case .prominent, .default:
+                textField.backgroundColor = color
+            @unknown default:
+                break
+            }
+        }
+    }
+    
+    func setPlaceholderTextColor(color: UIColor) {
+        if let textField = getSearchBarTextField() {
+            textField.attributedPlaceholder = NSAttributedString(string: self.placeholder != nil ? self.placeholder! : "", attributes: [NSAttributedString.Key.foregroundColor: color])
+        }
+    }
+    
+    private var activityIndicator: UIActivityIndicatorView? {
+        return textField?.leftView?.subviews.compactMap{ $0 as? UIActivityIndicatorView }.first
+    }
+    
+    var isLoading: Bool {
+        get {
+            return activityIndicator != nil
+        } set {
+            let _searchIcon = searchIcon
+            if newValue {
+                if activityIndicator == nil {
+                    let _activityIndicator = UIActivityIndicatorView(style: UIActivityIndicatorView.Style.medium)
+                    _activityIndicator.startAnimating()
+                    _activityIndicator.backgroundColor = UIColor.clear
+                    let clearImage = UIImage().imageWithPixelSize(size: CGSize.init(width: 14, height: 14)) ?? UIImage()
+                    self.setImage(clearImage, for: .search, state: .normal)
+                    textField?.leftViewMode = .always
+                    textField?.leftView?.addSubview(_activityIndicator)
+                    let leftViewSize = CGSize.init(width: 14.0, height: 14.0)
+                    _activityIndicator.center = CGPoint(x: leftViewSize.width/2, y: leftViewSize.height/2)
+                }
+            } else {
+                self.setImage(_searchIcon, for: .search, state: .normal)
+                activityIndicator?.removeFromSuperview()
+            }
+        }
+    }
+}
